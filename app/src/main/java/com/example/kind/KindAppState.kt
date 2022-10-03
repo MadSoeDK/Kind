97a8e841d6c964dbe8cff0d612ed3a83e52123cc
Mads Sørensen <s215805@student.dtu.dk>
2022-10-03 10:01:49 +0200

    Fixed navigation

--- app/src/main/java/com/example/kind/KindAppState.kt
+++ app/src/main/java/com/example/kind/KindAppState.kt
@@ -1,13 +1,7 @@
 package com.example.kind
 
-import android.content.Context
-import android.graphics.drawable.Icon
-import androidx.compose.material.ScaffoldState
-import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
@@ -18,24 +12,24 @@ sealed class Screen(val route: String) {
 }
 
 class KindAppState(
-    private val context: Context,
-    val scaffoldState: ScaffoldState,
     val navController: NavHostController
 ) {
 
     fun navigateToBottomBarRoute(route: String) {
-        navController.navigate(route) {
-            // Pop up to the start destination of the graph to
-            // avoid building up a large stack of destinations
-            // on the back stack as users select items
-            popUpTo(navController.graph.findStartDestination().id) {
-                saveState = true
+        if (route != navController.currentDestination?.route) {
+            navController.navigate(route) {
+                // Pop up to the start destination of the graph to
+                // avoid building up a large stack of destinations
+                // on the back stack as users select items
+                popUpTo(navController.graph.findStartDestination().id) {
+                    saveState = true
+                }
+                // Avoid multiple copies of the same destination when
+                // reselecting the same item
+                launchSingleTop = true
+                // Restore state when reselecting a previously selected item
+                restoreState = true
             }
-            // Avoid multiple copies of the same destination when
-            // reselecting the same item
-            launchSingleTop = true
-            // Restore state when reselecting a previously selected item
-            restoreState = true
         }
     }
 
@@ -43,9 +37,7 @@ class KindAppState(
 
 @Composable
 fun rememberKindAppState (
-    context: Context = LocalContext.current,
-    scaffoldState: ScaffoldState = rememberScaffoldState(),
     navController: NavHostController = rememberNavController()
-) = remember (context, scaffoldState, navController) {
-    KindAppState(context, scaffoldState, navController)
+) = remember (navController) {
+    KindAppState(navController)
 }
