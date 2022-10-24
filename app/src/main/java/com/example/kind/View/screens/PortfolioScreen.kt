2c6713bbcbde6ee6cad899abff1058ea0a7c820e
MarkNielsen00 <mpnvip@gmail.com>
2022-10-24 16:20:22 +0200

    optimized table in Portfolio

--- app/src/main/java/com/example/kind/View/screens/PortfolioScreen.kt
+++ app/src/main/java/com/example/kind/View/screens/PortfolioScreen.kt
@@ -24,10 +24,12 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.kind.View.composables.PieChart
 import com.example.kind.View.home.composables.HeaderAndText
 import com.example.kind.Model.Portfolio
+import com.example.kind.View.theme.Typography
 import com.example.kind.ViewModel.PortfolioViewModel
 
 @Composable
 fun PortfolioScreen(viewModel: PortfolioViewModel) {
+
     Column(
         modifier = Modifier.verticalScroll(rememberScrollState())
     ) {
@@ -40,7 +42,11 @@ fun PortfolioScreen(viewModel: PortfolioViewModel) {
                 Color(0xFF8b0a50)
             )
         )
-        show_table()
+        Text(text = "Your charities", fontSize = 24.sp, textAlign = TextAlign.Center,
+            modifier = Modifier.padding(6.dp), fontWeight = FontWeight.Black,
+            color = Typography.h1.color)
+
+        tableValues()
     }
 }
 
@@ -53,76 +59,6 @@ fun PortfolioScreenPreview() {
         viewModel
     )
 }
-
-/**
-AUTHOR:
-https://alexzh.com/jetpack-compose-building-grids/?fbclid=IwAR2i7ui8k_DuxFeacHb41USl_0v8yaw9p-MHTAhTZxCueqXCsqzZGG3F55Y
- */
-@Composable
-fun show_table() {
-    val portfolio = listOf(
-        Portfolio("Røde Kors", 21, 100, 150),
-        Portfolio("UNICEF", 35, 100, 250),
-        Portfolio("Mødrehjælpen", 26, 100, 540),
-        Portfolio("Julehjælpen", 32, 100, 300)
-    )
-    val cellWidth: (Int) -> Dp = { index ->
-        when (index) {
-            2 -> 165.dp
-            3 -> 165.dp
-            4 -> 150.dp
-            else -> 105.dp
-        }
-    }
-    val headerCellTitle: @Composable (Int) -> Unit = { index ->
-        val value = when (index) {
-            0 -> "Organization"
-            1 -> "%"
-            2 -> "Next month"
-            3 -> "Total"
-            else -> ""
-        }
-
-        Text(
-            text = value,
-            fontSize = 16.sp,
-            textAlign = TextAlign.Center,
-            modifier = Modifier.padding(2.dp, 0.dp),
-            maxLines = 1,
-            overflow = TextOverflow.Ellipsis,
-            fontWeight = FontWeight.Black,
-            textDecoration = TextDecoration.Underline
-        )
-    }
-    val cellText: @Composable (Int, Portfolio) -> Unit = { index, item ->
-        val value = when (index) {
-            0 -> item.organization
-            1 -> item.pct.toString()
-            2 -> item.spend.toString()
-            3 -> item.total.toString()
-            else -> ""
-        }
-
-        Text(
-            text = value,
-            fontSize = 16.sp,
-            textAlign = TextAlign.Center,
-            modifier = Modifier.padding(2.dp, 0.dp),
-            maxLines = 1,
-            overflow = TextOverflow.Ellipsis,
-        )
-    }
-
-    Table(
-        columnCount = 4,
-        cellWidth = cellWidth,
-        data = portfolio,
-        headerCellContent = headerCellTitle,
-        cellContent = cellText
-    )
-}
-
-
 @Composable
 fun <T> Table (
     columnCount: Int,
@@ -142,7 +78,7 @@ fun <T> Table (
                 Column {
                     (0..data.size).forEach { index ->
                         Surface(
-                            border = BorderStroke(1.dp, Color.LightGray),
+                            border = BorderStroke(1.dp, Color.LightGray ),
                             contentColor = Color.Transparent,
                             modifier = Modifier.width(cellWidth(columnIndex))
                         ) {
@@ -157,4 +93,69 @@ fun <T> Table (
             }
         }
     }
+}
+
+@Composable
+fun tableValues() {
+    val portfolio = listOf(
+        Portfolio("Røde Kors", 121, 100, 150),
+        Portfolio("UNICEF", 35, 100, 250),
+        Portfolio("Mødrehjælpen", 26, 101, 540),
+        Portfolio("Julehjælpen", 32, 100, 125),
+    )
+    val cellWidth: (Int) -> Dp = { index ->
+        when (index) {
+            0 -> 130.dp
+            1 -> 40.dp
+            2 -> 90.dp
+            3 -> 65.dp
+            else -> 70.dp
+        }
+    }
+    val headerCellTitle: @Composable (Int) -> Unit = { index ->
+        val value = when (index) {
+            0 -> "Organization"
+            1 -> "%"
+            2 -> "Next month"
+            3 -> "Total"
+            else -> ""
+        }
+
+        Text(
+            text = value,
+            fontSize = 14.sp,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.padding(6.dp),
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+            fontWeight = FontWeight.Black,
+            textDecoration = TextDecoration.Underline
+        )
+    }
+    val cellText: @Composable (Int, Portfolio) -> Unit = { index, item ->
+        val value = when (index) {
+            0 -> item.organization
+            1 -> item.pct.toString()
+            2 -> item.spend.toString()
+            3 -> item.total.toString()
+            else -> ""
+        }
+        Text(
+            text = value,
+            fontSize = 14.sp,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.padding(6.dp),
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+        )
+
+    }
+
+    Table(
+        columnCount = 4,
+        cellWidth = cellWidth,
+        data = portfolio,
+        headerCellContent = headerCellTitle,
+        cellContent = cellText
+    )
 }
