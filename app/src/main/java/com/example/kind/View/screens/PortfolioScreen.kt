be099eafa47cab180df8b8cfe6d4e490704a7a7b
MarkNielsen00 <mpnvip@gmail.com>
2022-10-24 09:28:40 +0200

    added preview to PortfolioScreen.kt

--- app/src/main/java/com/example/kind/View/screens/PortfolioScreen.kt
+++ app/src/main/java/com/example/kind/View/screens/PortfolioScreen.kt
@@ -1,7 +1,25 @@
 package com.example.kind.View.screens
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.kind.View.home.composables.HomeScreen
+import com.example.kind.ViewModel.HomeViewModel
 import com.example.kind.ViewModel.PortfolioViewModel
 
 fun PortfolioScreen(viewModel: PortfolioViewModel) {
 
+
+
+    //TABLE
+
+
+}
+@Composable
+@Preview(showBackground = true)
+fun PortfolioScreenPreview() {
+    val viewModel = viewModel<PortfolioViewModel>()
+    PortfolioScreen(
+        viewModel
+    )
 }
