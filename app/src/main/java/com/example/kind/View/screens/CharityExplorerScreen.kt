52388c97bfc0d7a8e808222cd3eced563f688c86
AugustHjortholm <s215800@dtu.dk>
2022-10-27 14:00:40 +0200

    Implemented material3

--- app/src/main/java/com/example/kind/View/screens/CharityExplorerScreen.kt
+++ app/src/main/java/com/example/kind/View/screens/CharityExplorerScreen.kt
@@ -3,8 +3,8 @@ package com.example.kind.View.home.composables
 import androidx.compose.foundation.ExperimentalFoundationApi
 import com.example.kind.R
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.GridCells
-import androidx.compose.foundation.lazy.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -19,7 +19,7 @@ fun CharityExplorerScreen(
 ) {
     Column {
         CharityHeaderAndSubsectionText(Title = "Charity Explorer", Subtitle = "Get to know other charities better", Categories = arrayOf("Health", "Disasters", "Climate", "Welfare", "Children Care"))
-        LazyVerticalGrid(cells = GridCells.Fixed(2), content = {
+        LazyVerticalGrid(columns = GridCells.Fixed(2), content = {
             items(10 /*Needs to be adaptive based on the amount of organizations*/) {i -> KindCharityCard(
                 Title = "Red Cross",
                 Body = "Lorem ipsum dolor sit amet, consectetur adipiscing elit...",
