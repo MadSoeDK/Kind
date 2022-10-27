b1d99484748a70161496b4420cb620e47ec092cd
Mads Sørensen <s215805@student.dtu.dk>
2022-10-27 14:50:15 +0200

    Added height to vertical grid to avoid error

--- app/src/main/java/com/example/kind/View/screens/ExplorerScreen.kt
+++ app/src/main/java/com/example/kind/View/screens/ExplorerScreen.kt
@@ -6,8 +6,10 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.kind.View.composables.CharityHeaderAndSubsectionText
 import com.example.kind.View.composables.KindCharityCard
 import com.example.kind.ViewModel.ExplorerViewModel
@@ -18,30 +20,16 @@ fun ExplorerScreen(
     viewModel: ExplorerViewModel
 ) {
     Column {
+        //TODO: Seperate categories composable
         CharityHeaderAndSubsectionText(Title = "Charity Explorer", Subtitle = "Get to know other charities better", Categories = arrayOf("Health", "Disasters", "Climate", "Welfare", "Children Care"))
-        LazyVerticalGrid(columns = GridCells.Fixed(2), content = {
-            items(10 /*Needs to be adaptive based on the amount of organizations*/) {i -> KindCharityCard(
+        LazyVerticalGrid(columns = GridCells.Fixed(2), Modifier.height(500.dp),content = {
+            items(10 /*TODO: Needs to be adaptive based on the amount of organizations*/) {i -> KindCharityCard(
                 Title = "Red Cross",
                 Body = "Lorem ipsum dolor sit amet, consectetur adipiscing elit...",
                 OrganizationIcon = painterResource(id = R.drawable.screenshot20220914071147),
                 Category = null
             )}
         })
-
-        /*Row() {
-            KindCharityCard(Title = "Red Cross", Body = "Lorem ipsum dolor sit amet, consectetur adipiscing elit...", OrganizationIcon = painterResource(id = R.drawable.screenshot20220914071147))
-            KindCharityCard(Title = "Doctors", Body = "Lorem ipsum dolor sit amet, consectetur adipiscing elit...", OrganizationIcon = painterResource(id = R.drawable.screenshot20220914071147))
-        }
-        Row() {
-            KindCharityCard(Title = "Red Cross", Body = "Lorem ipsum dolor sit amet, consectetur adipiscing elit...", OrganizationIcon = painterResource(id = R.drawable.screenshot20220914071147))
-            KindCharityCard(Title = "Doctors", Body = "Lorem ipsum dolor sit amet, consectetur adipiscing elit...", OrganizationIcon = painterResource(id = R.drawable.screenshot20220914071147))
-        }
-        Row() {
-            KindCharityCard(Title = "Red Cross", Body = "Lorem ipsum dolor sit amet, consectetur adipiscing elit...", OrganizationIcon = painterResource(id = R.drawable.screenshot20220914071147))
-            KindCharityCard(Title = "Doctors", Body = "Lorem ipsum dolor sit amet, consectetur adipiscing elit...", OrganizationIcon = painterResource(id = R.drawable.screenshot20220914071147))
-        }
-
-         */
     }
 }
 
