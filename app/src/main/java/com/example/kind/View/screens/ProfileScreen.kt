eb89a1164d587d6a555b1b4e791bf9627c375a1e
Mads Sørensen <s215805@student.dtu.dk>
2022-10-27 11:20:28 +0200

    started working on profilescreen

--- app/src/main/java/com/example/kind/View/screens/ProfileScreen.kt
+++ app/src/main/java/com/example/kind/View/screens/ProfileScreen.kt
@@ -1,16 +1,34 @@
 package com.example.kind.View.profile
 
-import androidx.compose.material.Button
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.TextField
+
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.kind.View.home.composables.HeaderAndText
 import com.example.kind.ViewModel.ProfileViewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ProfileScreen(
     viewModel: ProfileViewModel,
     onButtonClick: () -> Unit
 ) {
-    Button(onClick = onButtonClick) {
-        Text(text = viewModel.getButtonText())
+    HeaderAndText(donatedAmountProvider = "Profile info", welcomeText = "", horizontalAlignment = Alignment.Start)
+    TextField(value = "Helloworld", onValueChange = { handleChange("Change") })
+
+}
+
+fun handleChange(input: String) {
+
+}
+
+@Composable
+@Preview(showBackground = true)
+fun ProfileScreenPreview() {
+    ProfileScreen(viewModel = ProfileViewModel()) {
+
     }
 }
