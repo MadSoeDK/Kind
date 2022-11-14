08af2cee406a7d9548cec197ca643550e69e0660
Mads Sørensen <s215805@student.dtu.dk>
2022-11-14 11:51:18 +0100

    Added portfolio FAB again

--- app/src/main/java/com/example/kind/KindApp.kt
+++ app/src/main/java/com/example/kind/KindApp.kt
@@ -2,27 +2,34 @@ package com.example.kind
 
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.example.kind.View.screens.PortfolioScreen
 import com.example.kind.ViewModel.*
 import com.example.kind.view.home.composables.ExplorerScreen
 import com.example.kind.view.home.composables.HomeScreen
 import com.example.kind.view.profile.ProfileScreen
 import com.example.kind.view.screens.LoginScreen
-import com.example.kind.view.screens.PortfolioScreen
+import com.example.kind.view.theme.Typography
 
 sealed class Screen(val route: String, var icon: ImageVector) {
     object Login : Screen("login", Icons.Filled.Favorite)
@@ -56,6 +63,7 @@ fun KindApp() {
         composable(Screen.Portfolio.route) {
             Screen(
                 NavigationBar = { KindNavigationBar(viewModel = viewModel) },
+                FloatingActionButton = { EditPortfolioFAB() },
                 content = { PortfolioScreen(PortfolioViewModel()) }
             )
         }
@@ -78,10 +86,12 @@ fun KindApp() {
 @Composable
 fun Screen(
     NavigationBar: @Composable () -> Unit = {},
+    FloatingActionButton: @Composable () -> Unit = {},
     content: @Composable (PaddingValues) -> Unit
 ) {
     Scaffold(
         bottomBar = { NavigationBar() },
+        floatingActionButton = { FloatingActionButton() },
         content = content
     )
 }
@@ -112,4 +122,24 @@ fun KindNavigationBar(
             )
         }
     }
+}
+
+@Composable
+fun EditPortfolioFAB() {
+    FloatingActionButton(
+        onClick = { /*TODO*/ },
+        content = {
+            Icon(
+                Icons.Filled.Edit,
+                contentDescription = null,
+                tint = Color.White,
+                modifier = Modifier
+                    .width(30.dp)
+                    .height(30.dp),
+
+                )
+        },
+        containerColor = Typography.headlineLarge.color,
+        shape = RoundedCornerShape(10.dp)
+    )
 }
