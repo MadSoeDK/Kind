1059a3bb8abe5d4da6c4f720b22ed2f04c954ac8
EspenUlff <Espen.larp@gmail.com>
2022-11-14 14:53:38 +0100

    added TODO's and comments cleaned imports

--- app/src/main/java/com/example/kind/view/composables/LoginHeader.kt
+++ app/src/main/java/com/example/kind/view/composables/LoginHeader.kt
@@ -1,25 +1,18 @@
 package com.example.kind.view.composables
 
-import android.os.Bundle
-import android.text.style.BackgroundColorSpan
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.drawBehind
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.kind.R
-import com.example.kind.view.theme.KindTheme
-import com.example.kind.view.theme.background
 import com.example.kind.view.theme.primary
 
 /*class LoginHeader : ComponentActivity() {
