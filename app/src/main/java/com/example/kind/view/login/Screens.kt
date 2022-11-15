fc363d5044d324b620d9fa983e7703a98c15c87e
Mads Sørensen <s215805@student.dtu.dk>
2022-11-15 15:12:22 +0100

    Added image

--- app/src/main/java/com/example/kind/view/login/Screens.kt
+++ app/src/main/java/com/example/kind/view/login/Screens.kt
@@ -1,17 +1,29 @@
 package com.example.kind.view.login
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.navigation.NavController
+import com.example.kind.R
 
 @Composable
 fun StartScreen(
     navController: NavController
 ) {
     Column {
-        Text(text = "Start screen")
+        Text(text = "beKind")
+        Image(
+            painterResource(
+                id = R.drawable.screenshot20220914071147),
+                contentDescription = "",
+                modifier = Modifier.fillMaxWidth(),
+        )
+        Text(text = "Start building your portfolio of kindness today!")
         Button(onClick = { navController.navigate("signup") }) {
             Text(text = "Signup")
         }
