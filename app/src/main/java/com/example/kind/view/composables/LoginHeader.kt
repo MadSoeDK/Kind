e0f7cb5ea15b8e22c4c51e8134a8c3f5aaf8979c
EspenUlff <s215831@student.dtu.dk>
2022-11-17 13:53:19 +0100

    Implementation of LoginScreen

--- app/src/main/java/com/example/kind/view/composables/LoginHeader.kt
+++ app/src/main/java/com/example/kind/view/composables/LoginHeader.kt
@@ -1,19 +1,14 @@
 package com.example.kind.view.composables
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.kind.R
-import com.example.kind.view.theme.primary
 
 @Composable
 fun LoginHeader(size: Int) {
