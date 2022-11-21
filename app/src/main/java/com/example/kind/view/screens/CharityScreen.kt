554ce6fc34c9b2c1786a5e3f0e25519606236bbe
Mads Sørensen <s215805@student.dtu.dk>
2022-11-21 11:04:43 +0100

    cleanup

--- app/src/main/java/com/example/kind/view/screens/CharityScreen.kt
+++ app/src/main/java/com/example/kind/view/screens/CharityScreen.kt
@@ -4,6 +4,10 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowLeft
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -22,74 +26,51 @@ import com.example.kind.view.home.composables.SmallHeaderAndText
 @Composable
 fun CharityScreen(
     viewModel: CharityViewModel,
-    /*donorAmount: String,
-    donationAmount: String,
-    organizationName: String,
-    organizationTheme: String*/
-)
-{
+) {
     Column(horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier
             .fillMaxHeight()
             .fillMaxWidth()
             .background(color = Color.White)
             .padding(0.dp, 5.dp))
-
     {
-
-        // Back Button
-        Box(
-            modifier = Modifier
-                .padding(10.dp, 5.dp)
-                .clip(CircleShape)
-                .background(color = Color.Gray)
-                .width(50.dp)
-                .height(40.dp)
-                .align(Alignment.Start)
-        )
         // Background
         Box(modifier = Modifier
             .background(color = Color.LightGray)
             .fillMaxWidth()
             .height(200.dp)
             .align(Alignment.CenterHorizontally)
-        )
-        {
-
-            Row(modifier = Modifier
-                .align(Alignment.Center)) {
+        ) {
+            IconButton(onClick = { viewModel.navController.popBackStack() }) {
+                Icon(Icons.Filled.KeyboardArrowLeft, contentDescription = "Back")
+            }
+            Row(modifier = Modifier.align(Alignment.BottomCenter)) {
                 Box(
                     modifier = Modifier
-                        .padding(0.dp, 10.dp)
                         .clip(CircleShape)
                         .background(color = Color.Gray)
-                        .width(150.dp)
-                        .height(150.dp)
+                        .width(75.dp)
+                        .height(75.dp)
                 )
             }
         }
-
 
         // Donations, Donors & Picture
         Row(modifier = Modifier
             .padding(10.dp, 5.dp)
             .align(Alignment.CenterHorizontally)
-        )
-        {
+        ) {
             Text(text = "150\n Donors",
                 color = subHeading,
                 fontSize = Typography.headlineMedium.fontSize,
                 textAlign = TextAlign.Center
             )
-
             Spacer(modifier = Modifier.padding(50.dp,0.dp))
-
             Text(text = "200\n Donations",
                 color = subHeading,
                 fontSize = Typography.headlineMedium.fontSize,
                 textAlign = TextAlign.Center
             )
-
         }
 
         // Header & Button
@@ -132,11 +113,10 @@ fun CharityScreen(
         LazyRow {
             viewModel.getArticles().forEach {
                 item {
-                    KindCard(titleProvider = "Charity " + it.id.toString(), subTitleProvier = it.header, onClick = { viewModel.navController.navigate("article" + it.id.toString()) })
+                    KindCard(titleProvider = "Article " + it.id.toString(), subTitleProvier = it.header, onClick = { viewModel.navController.navigate("article" + it.id.toString()) })
                 }
             }
         }
-
     }
 
 }
