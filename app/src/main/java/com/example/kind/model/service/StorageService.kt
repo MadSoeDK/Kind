b34367192b64ea7dc9df51f747ef574df60f5df8
Lucas Schoubye <lucasschoubye@gmail.com>
2023-01-03 12:51:54 +0100

    Added StorageService Interface method

--- app/src/main/java/com/example/kind/model/service/StorageService.kt
+++ app/src/main/java/com/example/kind/model/service/StorageService.kt
@@ -1,4 +1,28 @@
 package com.example.kind.model.service
 
 interface StorageService {
+
+    // Users
+    suspend fun addUser()
+    suspend fun deleteUser()
+
+    // Subscriptions
+    suspend fun addSubscription()
+    suspend fun deleteSubscription()
+    suspend fun modifySubscriptionAmount()
+    suspend fun modifySubscriptionPlan()
+
+    // Donations
+    suspend fun addDonation()
+    suspend fun deleteDonation()
+
+    // Charity
+    suspend fun increaseCharityDonationNumber()
+    suspend fun decreaseCharityDonationNumber()
+    suspend fun increaseCharityDonaterNumber()
+    suspend fun decreaseCharityDonaterNumber()
+    suspend fun addCharityAdministator()
+    suspend fun deleteCharityAdministrator()
+    suspend fun addCharityArticle()
+    suspend fun deleteArticle()
 }
