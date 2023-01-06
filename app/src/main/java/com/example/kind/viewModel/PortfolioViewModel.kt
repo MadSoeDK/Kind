deac94b9590b18359905c47ed2983fe59abafafe
Lucas Schoubye <lucasschoubye@gmail.com>
2023-01-06 14:25:24 +0100

    Created UserSubscription method

--- app/src/main/java/com/example/kind/viewModel/PortfolioViewModel.kt
+++ app/src/main/java/com/example/kind/viewModel/PortfolioViewModel.kt
@@ -5,11 +5,14 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.ViewModel
+import com.example.kind.Global
 import com.example.kind.model.Portfolio
 import com.example.kind.model.service.impl.StorageServiceImpl
 import com.example.kind.view.composables.FormState
 import com.example.kind.view.composables.KindTextField
 import com.example.kind.view.composables.Required
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 class PortfolioViewModel : ViewModel() {
 
@@ -52,6 +55,22 @@ class PortfolioViewModel : ViewModel() {
         }
 
         return percentages
+    }
+    fun getUserSubscriptions() : List<Portfolio>
+    {
+        storage = StorageServiceImpl()
+        var subscriptions = listOf<Portfolio>()
+
+        // Get the collection of subscriptions
+        runBlocking {
+            // launch a coroutine
+            launch {
+                val subRef = storage.getSubscription(Global.currentUser)
+                subscriptions += Portfolio("Støt Studenter",50.0f,10f,100f)
+            }
+        }
+
+        return subscriptions
     }
     fun getSpend() : Float {
         var spend = 0f
