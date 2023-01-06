ac552f9560c4050e2d1f57fd3dca34d897e61af4
Mads Sørensen <s215805@dtu.dk>
2023-01-06 14:36:48 +0100

    cleanup packages

--- app/src/main/java/com/example/kind/view/auth_screens/AuthenticationScreen.kt
+++ app/src/main/java/com/example/kind/view/auth_screens/AuthenticationScreen.kt
@@ -1,16 +1,12 @@
 package com.example.kind.view.auth_screens
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import androidx.navigation.NavController
-import com.example.kind.AuthenticationScreens
 import com.example.kind.view.composables.LoginHeader
 
 @Composable
