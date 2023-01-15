88d07cfbc3b855ac7c5d08c671a413bdde08cc89
Mads Sørensen <s215805@dtu.dk>
2023-01-15 01:20:53 +0100

    added PaymentConfigs on app launch

--- app/src/main/java/com/example/kind/MainActivity.kt
+++ app/src/main/java/com/example/kind/MainActivity.kt
@@ -4,13 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.example.kind.view.theme.KindTheme
+import com.stripe.android.PaymentConfiguration
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        PaymentConfiguration.init(
+            applicationContext,
+            BuildConfig.API_KEY
+        )
         setContent {
             KindTheme {
-                KindApp()
+                KindApp(this)
             }
         }
     }
