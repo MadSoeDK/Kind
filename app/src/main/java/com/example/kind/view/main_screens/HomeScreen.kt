fee837dc647093dc682b0b24dd71538def2259c8
MarkNielsen00 <mpnvip@gmail.com>
2023-01-16 10:21:21 +0100

    fixes in names

--- app/src/main/java/com/example/kind/view/main_screens/HomeScreen.kt
+++ app/src/main/java/com/example/kind/view/main_screens/HomeScreen.kt
@@ -1,6 +1,5 @@
 package com.example.kind.view.main_screens
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.material3.CircularProgressIndicator
@@ -11,15 +10,13 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Color.Companion.Yellow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.kind.HomeScreens
 import com.example.kind.view.composables.HeaderAndText
+import com.example.kind.view.composables.KindCard
 import com.example.kind.view.theme.*
 import com.example.kind.viewModel.HomeViewModel
-import com.example.kind.view.composables.KindCard
 
 @Composable
 fun HomeScreen(
