d2dea596f0d1cd2aad135e533e8783a5bcafc34e
EspenUlff <s215831@student.dtu.dk>
2023-01-16 12:22:26 +0100

    layout changes

--- app/src/main/java/com/example/kind/view/signup_screens/DonationAmountScreen.kt
+++ app/src/main/java/com/example/kind/view/signup_screens/DonationAmountScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.example.kind.view.composables.LoginHeader
 import com.example.kind.view.theme.Shapes
+import com.example.kind.viewModel.DonationFrequency
 
 //TODO set button to fixed size, ignoring the content
 @Composable
@@ -24,36 +25,64 @@ fun DonationAmountScreen(
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier
             .fillMaxSize()
-            .background(MaterialTheme.colorScheme.background),
+            .background(MaterialTheme.colorScheme.background)
     ) {
-        LoginHeader(size = 150, "How much would you like to donate")
-        listOf(50, 100, 200).forEach {
-            OutlinedButton(
-                onClick = { onOptionSelected(it) },
-                shape = Shapes.extraSmall,
-                modifier = Modifier
-                    .width(300.dp),
-                colors = ButtonDefaults.outlinedButtonColors(
-                    containerColor = if (isSystemInDarkTheme()) {
-                        if (it == selectedOption) MaterialTheme.colorScheme.secondaryContainer else MaterialTheme.colorScheme.outline
-                    } else {
-                        if (it == selectedOption) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.secondaryContainer
-                    },
-                    contentColor = if (isSystemInDarkTheme()) {
-                        if (it == selectedOption) MaterialTheme.colorScheme.onSecondaryContainer else MaterialTheme.colorScheme.onBackground
-                    } else {
-                        if (it == selectedOption) MaterialTheme.colorScheme.onPrimary else MaterialTheme.colorScheme.onSecondaryContainer
-                    },
-                )
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier
+                .fillMaxHeight()
+                .width(280.dp),
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
             ) {
-                Text(text = it.toString())
+                LoginHeader(size = 150, "How often will you make a donation")
+
+                listOf(50, 100, 200).forEach {
+                    Spacer(modifier = Modifier.height(15.dp))
+                    OutlinedButton(
+                        onClick = { onOptionSelected(it) },
+                        shape = Shapes.extraSmall,
+                        modifier = Modifier
+                            .width(300.dp),
+                        colors = ButtonDefaults.outlinedButtonColors(
+                            containerColor = if (isSystemInDarkTheme()) {
+                                if (it == selectedOption) MaterialTheme.colorScheme.secondaryContainer else MaterialTheme.colorScheme.outline
+                            } else {
+                                if (it == selectedOption) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.secondaryContainer
+                            },
+                            contentColor = if (isSystemInDarkTheme()) {
+                                if (it == selectedOption) MaterialTheme.colorScheme.onSecondaryContainer else MaterialTheme.colorScheme.onBackground
+                            } else {
+                                if (it == selectedOption) MaterialTheme.colorScheme.onPrimary else MaterialTheme.colorScheme.onSecondaryContainer
+                            },
+                        )
+                    ) {
+                        Text(text = it.toString())
+                    }
+                }
             }
-        }
-        Button(onClick = next) {
-            Text(text = "Continue")
-        }
-        Button(onClick = back) {
-            Text(text = "Back")
+
+            Column {
+                Row(
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Column(horizontalAlignment = Alignment.Start) {
+                        Button(onClick = back) {
+                            Text(text = "Back")
+                        }
+                    }
+
+                    Column(horizontalAlignment = Alignment.End) {
+                        Button(onClick = next) {
+                            Text(text = "Next →")
+                        }
+                    }
+                }
+                Spacer(modifier = Modifier.height(10.dp))
+            }
         }
     }
 }
--- app/src/main/java/com/example/kind/view/signup_screens/DonationFreqScreen.kt
+++ app/src/main/java/com/example/kind/view/signup_screens/DonationFreqScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import com.example.kind.view.composables.KindButton
 import com.example.kind.view.composables.LoginHeader
 import com.example.kind.view.theme.Shapes
 import com.example.kind.viewModel.DonationFrequency
@@ -25,37 +26,64 @@ fun DonationFreqScreen(
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier
             .fillMaxSize()
-            .background(MaterialTheme.colorScheme.background),
+            .background(MaterialTheme.colorScheme.background)
     ) {
-        LoginHeader(size = 150, "How often will you make a donation")
-
-        DonationFrequency.values().forEach {
-            OutlinedButton(
-                onClick = { onOptionSelected(it) },
-                shape = Shapes.extraSmall,
-                modifier = Modifier
-                    .width(300.dp),
-                colors = ButtonDefaults.outlinedButtonColors(
-                    containerColor = if (isSystemInDarkTheme()) {
-                        if (it == selectedOption) MaterialTheme.colorScheme.secondaryContainer else MaterialTheme.colorScheme.outline
-                    } else {
-                        if (it == selectedOption) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.secondaryContainer
-                    },
-                    contentColor = if (isSystemInDarkTheme()) {
-                        if (it == selectedOption) MaterialTheme.colorScheme.onSecondaryContainer else MaterialTheme.colorScheme.onBackground
-                    } else {
-                        if (it == selectedOption) MaterialTheme.colorScheme.onPrimary else MaterialTheme.colorScheme.onSecondaryContainer
-                    },
-                )
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier
+                .fillMaxHeight()
+                .width(280.dp),
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
             ) {
-                Text(text = it.toString())
+                LoginHeader(size = 150, "How often will you make a donation")
+
+                DonationFrequency.values().forEach {
+                    Spacer(modifier = Modifier.height(15.dp))
+                    OutlinedButton(
+                        onClick = { onOptionSelected(it) },
+                        shape = Shapes.extraSmall,
+                        modifier = Modifier
+                            .width(300.dp),
+                        colors = ButtonDefaults.outlinedButtonColors(
+                            containerColor = if (isSystemInDarkTheme()) {
+                                if (it == selectedOption) MaterialTheme.colorScheme.secondaryContainer else MaterialTheme.colorScheme.outline
+                            } else {
+                                if (it == selectedOption) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.secondaryContainer
+                            },
+                            contentColor = if (isSystemInDarkTheme()) {
+                                if (it == selectedOption) MaterialTheme.colorScheme.onSecondaryContainer else MaterialTheme.colorScheme.onBackground
+                            } else {
+                                if (it == selectedOption) MaterialTheme.colorScheme.onPrimary else MaterialTheme.colorScheme.onSecondaryContainer
+                            },
+                        )
+                    ) {
+                        Text(text = it.toString())
+                    }
+                }
             }
-        }
-        Button(onClick = next) {
-            Text(text = "Continue")
-        }
-        Button(onClick = back) {
-            Text(text = "Back")
+            
+            Column {
+                Row(
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Column(horizontalAlignment = Alignment.Start) {
+                        Button(onClick = back) {
+                            Text(text = "Back")
+                        }
+                    }
+
+                    Column(horizontalAlignment = Alignment.End) {
+                        Button(onClick = next) {
+                            Text(text = "Next →")
+                        }
+                    }
+                }
+                Spacer(modifier = Modifier.height(10.dp))
+            }
         }
     }
 }
