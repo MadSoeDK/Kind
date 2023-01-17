36a36c4ff625172583f53ffd15d0b840cf7f151a
Lucas Schoubye <lucasschoubye@gmail.com>
2023-01-17 09:28:29 +0100

    Articles navigation fix and 10 kr minimum donation when added

--- app/src/main/java/com/example/kind/model/service/impl/StorageServiceImpl.kt
+++ app/src/main/java/com/example/kind/model/service/impl/StorageServiceImpl.kt
@@ -63,7 +63,7 @@ class StorageServiceImpl(
         }
         // If not, then add it to your subscriptions
         if (!Subscribed) {
-            val subscription = Subscription(0.0, charityId, charityId, Timestamp(1, 1))
+            val subscription = Subscription(10.0, charityId, charityId, Timestamp(1, 1))
 
             database.collection("Users").document(currentUser!!.uid).collection("Subscriptions")
                 .add(subscription)
